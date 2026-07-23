6995b423fe1d0dfb697c863ff5699e6e8d831241
priya <priyankeshshivam@gmail.com>
2026-07-23 23:47:09 +0300

    firstCommit

---
+++ src/main/java/com/prishi/taskApp/domain/entity/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.prishi.taskApp.domain.entity;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/prishi/taskApp/domain/entity/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.prishi.taskApp.domain.entity;
+
+public enum TaskStatus {
+    OPEN,
+    COMPLETE
+}
